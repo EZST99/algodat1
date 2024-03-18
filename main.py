1217da2e76b478c3c24c0afc4da5c2fb3644a823
Dicle <dicle.bbr@gmail.com>
2024-03-18 20:51:29 +0100

    exe davor

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import csv
 import pandas as pd
 
 class HashTable:
-    def __init__(self):
+    # Hashtabelle für die Speicherung von Aktien
+    def _init_(self):
         self.MAX = 1301  # Maximale Anzahl an Aktien, Primzahl, damit die Verteilung der Aktien in der Hashtabelle gleichmäßiger ist
         self.arr = [None for i in range(self.MAX)]  # Leere Liste mit 100 Elementen erstellen
 
+    # Hash-Funktion, um den Index im Array zu erhalten
     def get_hash(self, key): 
         #print(f"Test get hash")
         h = 0
@@ -13,89 +15,72 @@ class HashTable:
             h += ord(char)  # ASCII-Wert jedes Zeichens im Schlüssel addieren
         return h % self.MAX  # Modulo, um den Index im Array zu erhalten
 
-    def __setitem__(self, key, value):
-        h = self.get_hash(key)
-        if self.arr[h] is None:
-            self.arr[h] = [(key, value)]
+    # Methode für das Einfügen von Werten in die Hashtabelle
+    def _setitem_(self, key, value):
+        h = self.get_hash(key) # Hash-Wert für den Schlüssel berechnen
+        if self.arr[h] is None: # Falls leer wird es direkt hinzugefügt
+            self.arr[h] = [(key, value)] 
         else:
-            # Starten Sie die quadratische Sondierung
+            # Berechnen der neuen Position mit quadratischer Sondierung
             j = 1
             while True:
-                # Berechnen Sie die neue Hash-Position mit quadratischer Sondierung
-                new_h = (h + j**2) % self.MAX
-                if self.arr[new_h] is None:
-                    self.arr[new_h] = [(key, value)]
-                    break
-                j += 1
+                new_h = (h + j**2) % self.MAX # neuer Hash-Wert durch quadratische Sondierung
+                if self.arr[new_h] is None: 
+                    self.arr[new_h] = [(key, value)] # Falls leer direkt hinzufügen
+                    break 
+                j += 1 
                 # Abbruchbedingung, um endlose Schleifen zu vermeiden
-                if j > self.MAX:
+                if j > self.MAX: 
                     print("Hash-Tabelle ist voll, konnte keinen freien Slot finden.")
                     break
-
-
-                             
-        '''found = False
-        # Durchlaufen der Liste am Index h, um den Schlüssel zu finden
-        for idx, element in enumerate(self.arr[h]): 
-            # Überprüfen, ob der Schlüssel bereits vorhanden ist
-            if len(element) == 2 and element[0] == key: 
-                self.arr[h][idx] = (key, value)  # Wenn ja, aktualisiere den Wert
-                found = True 
-                break
-        if not found:
-            self.arr[h].append((key, value))  # Wenn nicht gefunden, hänge das Element an die Liste an
-        '''
-    def __getitem__(self, key):
+           
+    # Methode zum Abrufen von Werten aus der Hashtabelle     
+    def _getitem_(self, key):
         h = self.get_hash(key)
-        j = 0  # Beginne bei 0, um den quadratischen Sondierungsprozess einzuleiten
-
+        j = 0  
         while j <= self.MAX:  # Fortsetzen, bis self.MAX Versuche erreicht sind
-            new_h = (h + j**2) % self.MAX
-            slot = self.arr[new_h]
-            if slot is not None:
+            new_h = (h + j**2) % self.MAX 
+            slot = self.arr[new_h] 
+            if slot is not None: 
                 for k, v in slot:
                     if k == key:
-                        return v  # Gefunden
+                        return v  # Gefunden 
             j += 1
 
         return None  # Nicht gefunden nach Durchlaufen aller Slots
 
-
-
-    def __delitem__(self, key):
+    # Methode zum Löschen von Werten aus der Hashtabelle
+    def _delitem_(self, key):
         h = self.get_hash(key)
-        j = 0  # Beginnen Sie mit 0, um den ursprünglichen Hash-Slot als ersten Schritt einzubeziehen
-        while j <= self.MAX:
-            new_h = (h + j**2) % self.MAX
-            if self.arr[new_h] is not None:
-                for i, (k, v) in enumerate(self.arr[new_h]):
+        j = 0  
+        while j <= self.MAX: # Fortsetzen, bis self.MAX Versuche erreicht sind
+            new_h = (h + j**2) % self.MAX  
+            if self.arr[new_h] is not None: 
+                for i, (k, v) in enumerate(self.arr[new_h]): # Durchlaufen aller Werte im Slot, enumerate gibt ein Tupel zurück, das den Index und das Element enthält
                     if k == key:
-                        del self.arr[new_h][i]  # Löschen des gefundenen Elements
-                        # Prüfen, ob die Liste jetzt leer ist, und wenn ja, setzen Sie sie auf None
-                        if not self.arr[new_h]:
-                            self.arr[new_h] = None
+                        self.arr[new_h] = None
                         return
             j += 1
         print(f"Element mit dem Schlüssel {key} wurde nicht gefunden.")
 
-                
+# Klasse für die Aktie          
 class Stock:
-    def __init__(self, name, wkn, symbol, course_data=[]):
+    def _init_(self, name, wkn, symbol, course_data=[]): 
         self.name = name
         self.wkn = wkn
         self.symbol = symbol
-        self.course_data = course_data  # Kursdaten für die Aktie
+        self.course_data = course_data
 
     def add_course_data(self, data):
         self.course_data.append(data)  # Kursdaten hinzufügen
 
+# Funktion zum Importieren von Kursdaten aus einer CSV-Datei
 def import_stock_data(filename):
-    # Kursdaten aus einer CSV-Datei importieren und als Liste von Tupeln zurückgeben
-    course_data = []
-    with open(filename, 'r') as file:
-        reader = csv.DictReader(file)
+    course_data = [] 
+    with open(filename, 'r') as file: # CSV-Datei öffnen
+        reader = csv.DictReader(file) # CSV-Datei als Dictionary lesen
         count = 0  # Zähler für importierte Kurswerte
-        for row in reader:
+        for row in reader: 
             if count < 30:  # Maximal 30 Kurswerte importieren
                 course_data.append((row['Date'], float(row['Open']), float(row['High']), 
                                     float(row['Low']), float(row['Close']), int(row['Volume']), 
@@ -105,7 +90,7 @@ def import_stock_data(filename):
                 break
     return course_data
 
-
+# Funktion um Stocks hinzu zu fügen
 def add_stock(hashtable):
     # Neue Aktie hinzufügen
     name = input("Name der Aktie: ")
@@ -114,12 +99,14 @@ def add_stock(hashtable):
     hashtable[symbol] = Stock(name, wkn, symbol) # Aktie wird mit dem Symbol als Schlüssel zur Hashtabelle hinzugefügt
     print(f"Aktie {name} hinzugefügt.") 
 
+# Funktion um Stocks zu löschen
 def delete_stock(hashtable):
     # Aktie aus der Hashtabelle löschen
     symbol = input("Symbol der Aktie zum Löschen: ")
     del hashtable[symbol] # Aktie mit dem Symbol als Schlüssel wird aus der Hashtabelle gelöscht
     print(f"Aktie {symbol} gelöscht.")
 
+# Funktion um Stocks zu suchen
 def search_stock(hashtable):
     # Nach einer Aktie in der Hashtabelle suchen
     symbol = input("Symbol der Aktie zum Suchen: ")
@@ -131,19 +118,19 @@ def search_stock(hashtable):
 
 def plot_stock(hashtable):
     # Kursdaten einer Aktie plotten
-    import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt # Bibliothek zum Plotten von Diagrammen
     symbol = input("Symbol der Aktie zum Plotten: ")
     stock = hashtable[symbol]
     if stock:
-        course_data = stock.course_data
-        dates = [data[0] for data in course_data]
-        close_values = [data[4] for data in course_data]
+        course_data = stock.course_data # Kursdaten der Aktie
+        dates = [data[0] for data in course_data] # Datumswerte
+        close_values = [data[4] for data in course_data] # Schlusskurswerte
         plt.plot(dates, close_values)
         plt.xticks(rotation=45, ha='right')  # X-Achsenbeschriftung anpassen
         plt.xlabel('Datum')
         plt.ylabel('Schlusskurs')
         plt.title(f'Schlusskursverlauf von {stock.name}')
-        plt.show()
+        plt.show() 
     else:
         print("Aktie nicht gefunden.")
 
@@ -152,17 +139,17 @@ def save_stock(hashtable):
     data = []
     for slot in hashtable.arr:
         if slot is not None:  # Überprüfen, ob der Slot nicht leer ist
-            for key, value in slot:  # Durchlaufen aller Schlüssel-Wert-Paare im Slot
+            for key, value in slot:  # Durchlaufen aller Werte im Slot
                 stock_data_formatted = [{'Date': x[0], 'Open': x[1], 'High': x[2], 'Low': x[3], 'Close': x[4], 'Volume': x[5], 'Adj Close': x[6]} for x in value.course_data]
                 data.append({'Name': value.name, 'WKN': value.wkn, 'Symbol': value.symbol, 'StockData': stock_data_formatted})
-    df = pd.DataFrame(data)
-    df.to_csv(f'{doc_name}.csv', index=False)
+    df = pd.DataFrame(data) # Daten in ein DataFrame umwandeln
+    df.to_csv(f'{doc_name}.csv', index=False) # DataFrame in eine CSV-Datei speichern
     print("Daten als {doc_name}.csv gespeichert.")  
 
 def load_stock(hashtable):
     filename = input("Dateiname zum Laden: ")
-    df = pd.read_csv(filename)
-    for _, row in df.iterrows():
+    df = pd.read_csv(filename) # CSV-Datei auslesen
+    for _, row in df.iterrows(): # Durchlaufen aller Zeilen im DataFrame 
         symbol = row['Symbol']
         # Erstelle ein Stock-Objekt mit den geladenen Daten
         stock = Stock(row['Name'], row['WKN'], row['Symbol'], row.get('StockData', []))
@@ -216,5 +203,5 @@ def main_menu():
             case _:
                 print("Ungültige Eingabe.")
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main_menu()
